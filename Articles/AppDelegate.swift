6b03e095370a07e151ac9a7d29c95de9ba6f5ca7
ShmeL11 <romakoval343@gmail.com>
2024-03-13 21:25:42 +0200

    no message

--- Articles/AppDelegate.swift
+++ Articles/AppDelegate.swift
@@ -10,10 +10,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+                let navController = UINavigationController(rootViewController: ViewController())
+                window?.rootViewController = navController
+                window?.makeKeyAndVisible()
         return true
     }
 
--- Articles/ViewController.swift
+++ Articles/ViewController.swift
@@ -9,11 +9,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome to My App"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        return label
+    }()
+
+    let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Next", for: .normal)
+        button.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    func setupUI() {
+        view.backgroundColor = .white
 
+        view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+
+        view.addSubview(nextButton)
+        NSLayoutConstraint.activate([
+            nextButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
+    @objc func nextButtonTapped() {
+        let nextVC = NextViewController()
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
+}
+
+class NextViewController: UIViewController {
+
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Next Screen"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        return label
+    }()
+
+    let backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Back", for: .normal)
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    func setupUI() {
+        view.backgroundColor = .white
+
+        view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+
+        view.addSubview(backButton)
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
+    @objc func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
 }
 
